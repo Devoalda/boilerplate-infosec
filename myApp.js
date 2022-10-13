97b8153c8828b7a0a561e63b7ccf6e2d4f4539ef
Glitch (luminous-enchanted-tarp) <none>
2022-10-13 03:51:53 +0000

    🦶🪠 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -1,61 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let helmet = require("helmet");
+const bcrypt = require("bcrypt");
 
 module.exports = app;
-const api = require('./server.js');
+const api = require("./server.js");
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: "deny" }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    },
+  })
+);
+app.use(
+  helmet({
+    frameguard: {
+      // configure
+      action: "deny",
+    },
+    contentSecurityPolicy: {
+      // enable and configure
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["style.com"],
+      },
+    },
+    dnsPrefetchControl: false, // disable
+  })
+);
+module.exports = app;
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use('/_api', api);
+//app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
